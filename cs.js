c714c4553deabc11631a4cff97904dc89dcf4111
SamsonKristineJoy <58771139+SamsonKristineJoy@users.noreply.github.com>
2020-02-12 20:21:40 +0800

    Add files via upload

---
+++ cs.js
@@ -0,0 +1,76 @@
+<script>
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+		var genre = [
+			'pop',
+			'rnb',
+			'hiphop',
+		
+		];
+
+        var artist = [
+            [""],
+            ["To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.","Ralph Waldo Emerson"],
+            ["Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.","H. Jackson Brown Jr., P.S. I Love You"]
+            ];
+
+
+var currentQuote = "";
+var currentAuthor = "";
+var randomquote = "";
+var randomcolor = "";
+
+function getQuote() {
+	randomquote = Math.floor(Math.random() * quotes.length);
+	randomcolor = Math.floor(Math.random() * colors.length);
+    currentQuote = quotes[randomquote][0];
+    currentAuthor = quotes[randomquote][1];
+	if (inIframe()) {
+		$('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=aLamm&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+	}
+
+	$(document).ready(function () {
+	    $('html body').animate({
+	        backgroundColor: colors[randomcolor],
+	        color: colors[randomcolor]
+	    }, 500);
+	    $('#newquote, .social-icons .fa-twitter').animate({ backgroundColor: colors[randomcolor] }, 500);
+			$('blockquote footer').animate({ color: colors[randomcolor] }, 500);	
+	    $('blockquote').animate({ borderLeftColor: colors[randomcolor] }, 500);
+	    $('#quotetext').animate({ opacity: 0 }, 500, function () {
+	        $(this).animate({ opacity: 1 }, 500);
+	        $(this).text(currentQuote);
+	    });
+	    $('#quotesource').animate({ opacity: 0 }, 500, function () {
+	        $(this).animate({ opacity: 1 }, 500);
+	        $(this).text(currentAuthor);
+	    });
+    });    
+}
+
+function openURL(url) {
+    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+
+getQuote();
+
+$(document).ready(function () {
+    $('#newquote').on('click', getQuote);
+    $('#tweetquote').on('click', function () {
+        if (!inIframe()) {
+            openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+        }
+    });
+});
+
+
+
+
+
+</script>
